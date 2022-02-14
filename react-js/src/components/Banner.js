2d1531b11d6cf4ad353e0ed858c4b271cb099e38
midou2022 <akatseegeesagk94@gmail.com>
2022-02-14 10:45:38 +0100

    commited

--- react-js/src/components/Banner.js
+++ react-js/src/components/Banner.js
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
 function Banner() {
-    const title = 'La maison jungle'
+    const title = 'TP-portail'
     return (
         <div className='lmj-banner'>
             <img src={logo} alt='La maison jungle' className='lmj-logo' />
